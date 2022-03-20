5d8336da6adefc0f80122751ed80e242eb5e9099
Marcel <sandermarcel1@t-online.de>
2022-03-20 18:04:59 +0100

    test

--- War_of_the_Summon/core/src/figures/Bauer.java
+++ War_of_the_Summon/core/src/figures/Bauer.java
@@ -5,8 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 
 
 public class Bauer extends Figures {
-
     public Bauer(int player, int x, int y,int live) {
-        super("Bauer",player, x, y, 3, 3,1,1, 2, new Texture("Bauer1.png"), new Texture("Bauer2.png"), new Texture("Bauer1click.png"), new Texture("Bauer2click.png"),2,false,false);
+        super("Bauer",player, x, y, 3, live,1,1, 2, new Texture("Bauer1.png"), new Texture("Bauer2.png"), new Texture("Bauer1click.png"), new Texture("Bauer2click.png"),2,false,false);
     }
 }
